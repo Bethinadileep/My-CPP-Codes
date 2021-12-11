6ce9929acb1c973ae0a7da5d5c4235ffe24cfb23
Bethina Dileep <56349269+Bethinadileep@users.noreply.github.com>
2021-12-11 16:22:16 +0530

    Create C++_Math.cpp

---
+++ C++_Math.cpp
@@ -0,0 +1,26 @@
+//codeby : Dileep
+
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+    //C++ Math
+    cout<<"Minimum Number"<<"\n";
+    cout<<min(10,5)<<"\n";
+    cout<<"Maximum Number"<<"\n";
+    cout<<max(10,5)<<"\n";
+    cout<<"Square Root Number"<<"\n";
+    cout<<sqrt(4)<<"\n";
+    cout<<"Round Number"<<"\n";
+    cout<<round(2.4)<<"\n";
+    cout<<"Logrithm Number"<<"\n";
+    cout<<log(2)<<"\n";
+    cout<<"Absolute Number"<<"\n";
+    cout<<abs(-45)<<"\n";
+    cout<<"Absolute Floating NUmber"
+    cout<<fabs(-2.34)<<"\n";
+  
+    return 0;
+}
