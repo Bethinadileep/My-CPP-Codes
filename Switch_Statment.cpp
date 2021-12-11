2903052166ba1544917f04b0ba448a69c05f6746
Bethina Dileep <56349269+Bethinadileep@users.noreply.github.com>
2021-12-11 15:26:56 +0530

    Create Switch_Statment.cpp

---
+++ Switch_Statment.cpp
@@ -0,0 +1,36 @@
+//codeby : Bethina Dileep
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int day = 4;
+    switch(day) {
+        case 1:
+            cout<<"Monday"<<"\n";
+            break;
+        case 2:
+            cout<<"Tuseday"<<"\n";
+            break;
+        case 3:
+            cout<<"Wednesday"<<"\n";
+            break;
+        case 4:
+            cout<<"Thursday"<<"\n";
+            break;
+        case 5:
+            cout<<"Friday"<<"\n";
+            break;
+        case 6:
+            cout<<"Saturday"<<"\n";
+            break;
+        case 7:
+            cout<<"Sunday"<<"\n";
+            break;
+        default:
+            cout<<"None of the Above Cases"<<"\n";
+            break; 
+    }
+    return 0;
+}
