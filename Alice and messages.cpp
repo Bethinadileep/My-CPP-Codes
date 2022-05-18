f8437363aff266d6b76e9efdee30eb71869ce4d2
Bethina Dileep <56349269+Bethinadileep@users.noreply.github.com>
2022-05-18 17:49:42 +0530

    Create Alice and messages.cpp

---
+++ Alice and messages.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int findMessages (int N, vector<string> A) {
+   // Write your code here
+   int ans=N;
+   set<string> st;
+
+    for(int i=0; i<N; i++) {
+        string s;
+        for(int j=0; j<A[i].size(); j++)
+            s += 'a' + ('z' - A[i][j]);
+        
+        if (st.find(s) != st.end())
+            ans -= 1;
+
+        st.insert(A[i]);
+    }
+
+    return ans;
+   
+}
+
+int main() {
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int N;
+    cin >> N;
+    vector<string> A(N);
+    for(int i_A = 0; i_A < N; i_A++)
+    {
+    	cin >> A[i_A];
+    }
+
+    int out_;
+    out_ = findMessages(N, A);
+    cout << out_;
+}
